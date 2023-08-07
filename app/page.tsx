514bab5b5d43a75d22d98430d139c54e1040cda8
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-08-07 18:26:27 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-[100vh] flex-col items-center justify-center p-24">
+      <div>
+        <h1 className="text-4xl">CodeBites</h1>
+      </div>
+    </main>
+  );
+}
