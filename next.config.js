bafb3cefaef500453731b09680516174a350a74c
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-08-10 19:55:19 +0530

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -37,4 +37,9 @@ module.exports = {
       },
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
