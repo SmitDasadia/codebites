0bc992f12ec4523ddc488f23d914838500da97f8
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-08-10 20:00:01 +0530

    update next confing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   
 }
 
@@ -37,9 +41,5 @@ module.exports = {
       },
     ],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  
 };
